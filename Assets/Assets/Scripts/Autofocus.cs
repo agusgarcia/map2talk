9525b81a4b72dd9cc768ac2c0f72a54df654ad66
Agustina <geminis_agus@hotmail.com>
2016-06-01 13:42:16 +0200

    first commit

---
+++ Assets/Assets/Scripts/Autofocus.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Vuforia {
+
+	public class Autofocus : MonoBehaviour {
+
+		void Start () 
+		{
+			VuforiaBehaviour.Instance.RegisterVuforiaStartedCallback(OnVuforiaStarted);
+			VuforiaBehaviour.Instance.RegisterOnPauseCallback(OnPaused);
+			Debug.Log ("start");
+		}
+
+		private void OnVuforiaStarted()
+		{
+			bool focusModeSet = CameraDevice.Instance.SetFocusMode( 
+				CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+
+			if (!focusModeSet) {
+				Debug.Log ("Failed to set focus mode (unsupported mode).");
+			} else {
+				Debug.Log ("Ok");		
+			}
+		}
+
+		private void OnPaused(bool paused)
+		{
+			if (!paused) // resumed
+			{
+				// Set again autofocus mode when app is resumed
+				CameraDevice.Instance.SetFocusMode (
+					CameraDevice.FocusMode.FOCUS_MODE_CONTINUOUSAUTO);
+			}
+		}
+	}
+}
---
+++ Assets/Assets/Scripts/ShowSquare.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ShowSquare : MonoBehaviour {
+
+
+	void OnMouseOver()
+	{
+		Debug.Log ("over");
+
+
+	}
+	void OnMouseEnter()
+	{
+		Debug.Log ("eenter");
+
+
+	}
+
+
+
+	void OnMouseExit()
+
+	{
+		Debug.Log ("exit");
+
+	}
+		
+
+
+
+}
